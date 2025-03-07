9d3031ca55fc0f09f0a964cd67f71781bc6a67d2
jehad-ismail <jehadanwar1@gmail.com>
2025-03-07 12:36:20 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "enter your name :  ";
+	cin >> name;
+	cout << "welcome mr " << name;
+
+
+
+
+
+
+
+
+
+	return 0;
+}
